4aaf3a69b9d16b815974e7b9e94a94b7c9741f8c
Lily Sakata <lsakata@csilvm-12.cs.ucsb.edu>
2026-02-01 20:51:45 -0800

    stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>  // for setprecision, setw -- the only ones you're allowed to use from this lib
 using namespace std;
 
+typedef int* IntPtr;
+
 // FUNCTION DECLARATIONS: YOU MUST USE THESE (do not remove):
 void bubbleSort(int *array, int size);
 double average(int *array, int size);
@@ -16,16 +18,44 @@ int main() {
     cout << setprecision(2);
 
     // MISSING CODE HERE (you can remove these comments)
-	// Get the number of grades to be entered from user
-    // Create a dynamic int array based on this information
-    // Get the user to enter all the grades
-    // Print results while calling the appropriate functions
+    int size;
+
+    cout << "Enter number of grades: ";
+
+    cin >> size;
+    
+    if (size < 1) {
+        cerr << "Error!\n";
+        exit(1);
+    }
+
+    IntPtr array = new int[size];
+
+    cout << "Enter grades (each on new line):" << endl;
+
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    } // for end
+
+    bubbleSort(array, size);
+    
+    double avg = average(array, size);
+    double med = median(array, size);
+    double std = stddev(array, size);
+
+    cout << "Here are some statistics:" << endl;
+    cout << setw(9) << "Average: " << avg << endl;
+    cout << setw(9) << "Median: " << med << endl;
+    cout << setw(9) << "StdDev: " << std << endl;
+
+    delete [] array;
 
     return 0;
-}
+
+} // main end
 
 // This is bubbleSort - it is given for you to use.
-// Pre-Condition:: It takes in a dynamic int array and its size
+// Pre-Condition: It takes in a dynamic int array and its size
 // Post-Condition: It sorts the array in ascending order of its values
 void bubbleSort(int *array, int size) {
   int temp;
@@ -41,10 +71,57 @@ void bubbleSort(int *array, int size) {
 }
 
 // FUNCTION DEFINITIONS
-//
-// Function 1 (get_average):
-// Pre-Conditions: 
-//
-// Function 2 (get_median):
-//
-// Function 3 (get_std_dev):
+
+// Function 1:
+// Pre-Condition: takes in a dynamic int array and its size
+// Post-Condition: returns the average of the indices 
+double average (int *array, int size) {
+
+    double sum (0), average;
+
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    } // for end
+
+    return average = sum/size;
+
+} // def end
+
+
+// Function 2:
+// Pre-Condition: takes in a dynamic int array and its size
+// Post-Condition: returns the median of the array 
+double median(int *array, int size) {
+    
+    int i = (size/2);
+
+    if (size % 2 != 0) {
+        double median = array[i];
+        return median;
+    }
+    else {
+        double sum = array[i - 1] + array[i];
+        double median = sum/2;
+        return median;
+    }
+
+} // def end
+
+// Function 3:
+// Pre-Condition: takes in a dynamic int array and its size
+// Post-Condition: returns the standard deviation of the array
+double stddev(int* array, int size) {
+
+    double sum(0);
+    double avg = average(array, size);
+
+    for (int i = 0; i < size; i++) {
+        sum += pow((array[i] - avg), 2.0);
+    } // for end
+    
+    double fraction = sum / (size - 1);
+
+    double std = pow(fraction, 0.5);
+    return std;
+
+} // def end
