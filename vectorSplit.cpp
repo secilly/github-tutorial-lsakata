a8dec05b5e886ea715cfef86c2b4092be99b423f
Lily Sakata <lsakata@csilvm-05.cs.ucsb.edu>
2026-02-06 15:04:56 -0800

    stylized

--- vectorSplit.cpp
+++ vectorSplit.cpp
@@ -21,23 +21,23 @@ int main() {
     // Call function split()
     vector<string> result = split(input, delimiter);
     
-    // Prints vector components with double quotes and commas separating them
+    // Prints vector string components wrapped in double quotes
     int rsize = result.size();
-    cout << "size: " << rsize << endl;
-    if (rsize > 1) {
+    
+    if (rsize > 1) { // multiple componenets
         cout << "The substrings are: ";
   
             for (int i = 0; i < rsize; i++) {
   
             cout << "\"" << result[i] << "\"";
    
-            if (i < rsize - 1) {
+            if (i < rsize - 1) { // comma printed up until last indice
             cout << ", ";
             }
 
         } // for end
     }
-    else {
+    else { // one component
         cout << "The substring is: " << "\"" << result[0] << "\"";
     }
 
@@ -46,38 +46,41 @@ int main() {
     return 0;
 }
 
-// FUNCTION DEFINITIONS
-
-// Pre-Condition: takes in two strings 
-// Post-Condition:
+// FUNCTION DEFINITION
+// Pre-Condition: takes in two strings (string to split and string to split by)
+// Post-Condition: returns a vector of strings that were split by the delimiter
 vector<string> split(string target, string delimiter) {
     
     vector<string> result;
     int size = target.length(), position = 0;
     
-    bool nosubstring = true;
+    bool nosubstring = true; // bool to check if delimiter exists in string
     for (int i = 0; i < size; i++) {
         if (target[i] == delimiter[0]) {
-            nosubstring = false;
+            nosubstring = false; // if the delimiter is found in target bool value changed
         }
     } // for end
 
-    if (nosubstring == true) {
-        result.push_back(target);
-        return result;
+    if (nosubstring == true) { // if delimiter never found in target
+        result.push_back(target); // target is added to result
+        return result; 
     }
     
+    // runs as long as the delimiter is found in target
     while ((position = target.find(delimiter)) != string::npos) {
 
         string substring = target.substr(0, position);
-
+        // empty strings are not added to result
         if (!substring.empty()) {
             result.push_back(substring);
         }
-
+        // substring and delimiter erased from target
         target.erase(0, position + delimiter.length());
 
     } // while end
+
+    // when no delimiter exists rest of target added to result
+    result.push_back(target);
 
     return result;
 
