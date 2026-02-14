c8baf2f185c1d58a1dfe13249f6ba06e538db3f1
lily <lilysakata3@gmail.com>
2026-02-13 22:19:36 -0800

    stylized

--- rhymes.cpp
+++ rhymes.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib> // for exit
+#include <iomanip> // for precision
 using namespace std;
 
 typedef string* StrPtr;
@@ -16,6 +17,9 @@ void cleanUp(string &word);
 bool compareWords(string word1, string word2);
 
 int main() {
+
+    cout << fixed << showpoint;
+    cout << setprecision(2);
 
     // Create input stream object
     ifstream lineInput;
@@ -36,11 +40,25 @@ int main() {
     StrPtr array = new string[size];
     StrPtr newArray = nullptr;
 
+    // runs until end of file
     while (!lineInput.eof()) {
         
+        // takes a line at a time
         getline(lineInput, line);
+    
+        // skips blank lines
+        if (line.empty()) {
+            continue;
+        }
+    
+        // finds and cleans up last word in the line
         string lastWord = findLastWord(line);
         cleanUp(lastWord);
+    
+        // skip if cleanUp erased word
+        if (lastWord.empty()) {
+            continue;
+        }
 
         // adds unformatted word to array
         array[i] = lastWord;
@@ -77,6 +95,7 @@ int main() {
         }
     } // for end 
 
+    // Prints specific lines depending on number of rhyming pairs
     double density = static_cast<double> (count) / size;
     if (count > 1) {
         cout << "There are " << count << " pairs of rhyming words." << endl;
@@ -99,7 +118,8 @@ int main() {
     delete [] array;
 
     return 0;
-}
+
+} // main end
 
 // FUNCTION DEFINITIONS
 // Pre-Condition: takes in a line of strings
@@ -119,8 +139,8 @@ string findLastWord(string line) {
 
 } // def end
 
-// Pre-Condition: 
-// Post-Condition: 
+// Pre-Condition: takes in a string by reference
+// Post-Condition: formats string to be only lowercase letters
 void cleanUp(string &word) {
 
     int size = word.size();
@@ -129,7 +149,7 @@ void cleanUp(string &word) {
         if (isalpha(word[i]) != 0) { // if letter converts to lower case
             word[i] = tolower(word[i]);
         } 
-        else { 
+        else { // deletes if not a letter
             word.erase(i, 1);
             i--;
             size--;
@@ -139,19 +159,19 @@ void cleanUp(string &word) {
 
 } // def end
 
-// Pre-Condition:
-// Post-Condition:
+// Pre-Condition: takes in two strings
+// Post-Condition: returns true if the last two letters in the words match each other
 bool compareWords(string word1, string word2) {
     
     int size1 = word1.size();
     int size2 = word2.size();
 
-    for (int i = 1; i <= 2; i++) { 
-        if (word1[size1 - i] != word2[size2 - i]) {
-            return false;
-        }
-    } // for i
-    
-    return true;
+   // compares the last two letters in each word together
+    if ( word1[size1 - 1] == word2[size2 - 1] && word1[size1 - 2] == word2[size2 - 2] ) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 } // def end
